66b8c20b028b444b412ca8331d0c2010415a4c5e
qwershu007 <qwershu007@163.com>
2022-03-31 19:33:45 +0100

    source_check

--- src/image_helper.cpp
+++ src/image_helper.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
-//#include <string>
+#include <string>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
 #include "image_helper.h"
 
-void image_helper::source_check()
+#define DEFAULT_IMAGE_PATH            RESOURCE_DIR //"Yoga1.jpg"
+
+//#include <iomanip>
+
+void image_helper::source_check(const std::string& input_name,cv::VideoCapture& cap)
 {
-
-std::cout<<"a"<< std::endl;
-
-
+	if (input_name.find(".jpg") != std::string::npos || input_name.find(".png") != std::string::npos) // read image ("string::npos" means not found)
+    {
+    	//cv::Mat img;
+    	img = cv::imread(input_name);
+    	if(img.empty())
+    	{
+    		std::cout << "Input source NOT found !!";
+    		//return 0;
+    	}
+    }   
+    else // read from camera
+    {
+        //int cam_id = -1;       
+        cam_id = stoi(input_name); // stoi : Convert string to integer, directly check the number in this string
+        
+        if(cam_id>=0)
+        {
+            cap=cv::VideoCapture(cam_id);
+        }
+        else
+        {
+            cap=cv::VideoCapture(input_name);
+        } 
+        
+        if (!cap.isOpened()) // Check if the camera is open successfully
+        {
+            std::cout<<"Open camera FAIL !! ";
+            //return 0;
+        }
+    }
+    //return 0;
 }
-
-
-
-void image_helper::key_check()
-{
-
-
-
-
-}
-
-
-
-
-void image_helper::cv_set() // temporarily
-{
-
-
-
-
-
-} 
