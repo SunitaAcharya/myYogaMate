3b94e43fc402814dd16b584960fee07160f85a51
Sunita Acharya <sunitaacharya74@gmail.com>
2022-04-01 15:44:06 +0100

    declaration added for setparam function

--- src/image_helper.h
+++ src/image_helper.h
@@ -17,6 +17,12 @@ private:
     int cam_id = -1;
 
     std::string image_input_name = DEFAULT_IMAGE_PATH;
+    
+    cv::Mat m_mat;
+    double m_zoom;
+    bool m_flip;
+    double fps;
+    std::string fps_result;
 
 public:
     image_helper() 
@@ -27,6 +33,7 @@ public:
     void source_check(const std::string& input_name,cv::VideoCapture& cap);
     bool key_check(cv::VideoCapture& cap);
     std::string getimgname();
+    void cv_setparam(cv::Mat& mat, double zoom, bool flip);
 
     ~image_helper()
     {
