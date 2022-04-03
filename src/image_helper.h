4bf01522e5209512755d711a664667f032a7b46d
qwershu007 <qwershu007@163.com>
2022-04-03 15:06:35 +0100

    source_check

--- src/image_helper.h
+++ src/image_helper.h
@@ -1,43 +1,28 @@
-//#pragma once
+#pragma once
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
+#define DEFAULT_IMAGE_PATH1            RESOURCE_DIR //"Yoga1.jpg"
 
-#define DEFAULT_IMAGE_PATH            RESOURCE_DIR
-
-class image_helper
+/* an interface for image_check function */
+class ImageSourceInterface 
 {
-protected:
-
-
-private:
-
-    cv::Mat img;
-    int cam_id = -1;
-
-    std::string image_input_name = DEFAULT_IMAGE_PATH;
-    
-    cv::Mat m_mat;
-    double m_zoom;
-    bool m_flip;
-    double fps;
-    std::string fps_result;
-
 public:
-    image_helper() 
-    {
+    virtual ~ImageSourceInterface() {}
+    virtual int source_check(const std::string& input_name,cv::VideoCapture& cap)=0;
+};
 
-    }
+/*This class is about checking the image source*/
+class image_check : public ImageSourceInterface
+{
+private:
+    cv::Mat img;
+    int cam_id = 0;
+public:
+    image_check() {}
+    ~image_check() {}
 
-    void source_check(const std::string& input_name,cv::VideoCapture& cap);
-    bool key_check(cv::VideoCapture& cap);
-    std::string getimgname();
-    void cv_setparam(cv::Mat& mat, double zoom, bool flip);
-
-    ~image_helper()
-    {
-
-    }
+    virtual int source_check(const std::string& input_name,cv::VideoCapture& cap);
 
 };
