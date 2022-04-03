03d2b94be96bbf370a1ef66e610443c197ebe665
qwershu007 <qwershu007@163.com>
2022-04-03 15:55:34 +0100

    Initialize

---
+++ src/image_processor.h
@@ -0,0 +1,28 @@
+#ifndef IMAGE_PROCESSOR_H_
+#define IMAGE_PROCESSOR_H_
+
+/* for general */
+
+#include <string>
+#include <vector>
+#include <array>
+#include <iostream>
+
+namespace cv 
+{
+    class Mat;
+};
+
+class ImageProcessor_Initialize
+{   
+    public:
+    typedef struct InputParam
+    {
+    char work_dir[256];
+    std::int32_t  num_threads;
+    } InputParam;
+
+    static int32_t Initialize(const InputParam& input_param);
+};
+
+#endif
