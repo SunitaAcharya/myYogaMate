3b594de66c098a12dd5da6ec768d56a71bea9949
Yuan <2672750z@student.gla.ac.uk>
2022-04-04 23:12:07 +0100

    add homepage function in .cpp

--- src/image_show.cpp
+++ src/image_show.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <mutex>
 #include <opencv2/highgui.hpp>
+#include <vector>
 
 #include "image_processor.h"
 #include "common_helper_cv.h"
@@ -148,4 +149,60 @@ int32_t image_show::img_process (std::string Source_path)
     return 0;   
 }
 
+/***** multiple images are shown in one windows *****/
+/***** set parameters for the images *****/
+void image_show::multipleImage(std::vector<cv::Mat> imgVector, cv::Mat& dst, int imgCols) 
+{
+    /***** set max pixel for every images *****/
+    const int MAX_PIXEL=600;
+    int imgNum = imgVector.size();
+    /***** set the longest side and resize to 600 pixel *****/
+    cv::Size imgOriSize = imgVector[0].size();
+    int imgMaxPixel = std::max(imgOriSize.height, imgOriSize.width);
+    /***** get ratio of the max pixel *****/
+    double prop = imgMaxPixel < MAX_PIXEL ?  (double)imgMaxPixel/MAX_PIXEL : MAX_PIXEL/(double)imgMaxPixel;
+    /***** show the fixed size of images *****/
+    cv::Size imgStdSize(imgOriSize.width * prop, imgOriSize.height * prop);
+    /***** standard image *****/
+    cv::Mat imgStd;
+    /***** start from (0,0) coordinate *****/
+    cv::Point2i location(0, 0);
+    /***** create image window *****/
+    cv::Mat imgWindow(imgStdSize.height*((imgNum-1)/imgCols + 1), imgStdSize.width*imgCols, imgVector[0].type());
+    for (int i=0; i<imgNum; i++)
+    {
+        location.x = (i%imgCols)*imgStdSize.width;
+        location.y = (i/imgCols)*imgStdSize.height;
+        cv::resize(imgVector[i], imgStd, imgStdSize, prop, prop, cv::INTER_LINEAR);
+        imgStd.copyTo( imgWindow( cv::Rect(location, imgStdSize) ) );
+    }
+    dst = imgWindow;
+}
 
+/***** set paths of the images *****/
+/***** show multiple images *****/
+void image_show::homepage()
+{
+    /***** create images *****/
+    cv::Mat image1 = cv::imread(WORK_DIR "yogapose1.jpg");
+    cv::Mat image2 = cv::imread(WORK_DIR "yogapose2.jpg");
+    cv::Mat image3 = cv::imread(WORK_DIR "yogapose3.jpg");
+    cv::Mat image4 = cv::imread(WORK_DIR "yogapose4.jpg");
+    cv::Mat image5 = cv::imread(WORK_DIR "yogapose5.jpg");
+    cv::Mat image6 = cv::imread(WORK_DIR "yogapose6.jpg");
+    cv::Mat dst;
+
+    imageVector.push_back(image1);
+    imageVector.push_back(image2);
+    imageVector.push_back(image3);
+    imageVector.push_back(image4);
+    imageVector.push_back(image5);
+    imageVector.push_back(image6);
+    /***** set the window parameters*****/
+    multipleImage(imageVector, dst, 3);
+    /***** set image window *****/
+    cv::namedWindow("My Yoga Mate");
+    cv::imshow("My Yoga Mate", dst);
+    std::cout << "Please press any key to start" << std::endl;
+    cv::waitKey(0);
+}
