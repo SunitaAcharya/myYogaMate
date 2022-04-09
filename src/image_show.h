57caba1271c8911c9d0f7d2151e6d98ca0f92cec
siyu0415 <2655583L@student.gla.ac.uk>
2022-04-09 21:27:33 +0100

    callback function

--- src/image_show.h
+++ src/image_show.h
@@ -7,17 +7,37 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
+class IMAGEcallback{
+    public:
+    virtual void yoga_thread(std::string cam_path, std::string img_path) = 0;
+};
+
 class image_show
 {
     private:
         static std::string m_input_name_img;
-   	std::vector<cv::Mat> imageVector;
+        std::vector<cv::Mat> imageVector;
+
+        std::thread* imgThread = nullptr;
+        int running = 0;
+        IMAGEcallback* imagecallback = nullptr;
+        void run();
+        static void exec(image_show* imageshow) {
+		imageshow->run();
+	    }
+
     public:    
         image_show() { }
-        ~image_show() { }
+        ~image_show() { stop(); }
         int32_t img_process(std::string Source_path);
         void multipleImage(std::vector<cv::Mat> imgVector, cv::Mat& dst, int imgCols);
         void homepage();
+        
+        void registerCallback(IMAGEcallback* cb);
+        void unRegisterCallback();
+        void start();
+        void stop();
+
 };
 
 class camera_show
