98f4166f413369def7edfaf9084f13a54837f445
siyu0415 <2655583L@student.gla.ac.uk>
2022-04-14 11:00:30 +0100

    add comment

--- src/image_processor.h
+++ src/image_processor.h
@@ -1,8 +1,16 @@
+/**
+*@file image_processor.h
+*@brief classes about analysing images  
+*
+*@copyright Copytight (C) 2022
+*
+*
+*/
+
 #ifndef IMAGE_PROCESSOR_H_
 #define IMAGE_PROCESSOR_H_
 
 /* for general */
-
 #include <string>
 #include <vector>
 #include <array>
@@ -13,11 +21,20 @@ namespace cv
     class Mat;
 };
 
+/**
+ * @brief This file is about analyzing the images from the specific source and camera.
+ * This file includes all the steps of analyzing the image.
+ * Before analyzing the image, initialize the image.
+ * Then check if the image is initialized successfully by using the function 'Finalize'.
+ * Next begin to analyze the images.
+ * 
+*/
+
 class ImageProcessor_Initialize
 {   
 public:
 
-typedef struct InputParam
+typedef struct InputParam // define new InputParam type struct with parameters work_dir and num_threads
 {
     char work_dir[256];
     std::int32_t  num_threads;
@@ -29,12 +46,12 @@ typedef struct InputParam
 class ImageProcessor_Process
 {   
 private:
-    double angle_image[8]={0,0,0,0,0,0,0,0};
-    double angle_camera[8];
+    double angle_image[8]={0,0,0,0,0,0,0,0}; // angle check for image pose
+    double angle_camera[8]; // angle check for users' pose from camera
     int Learner[12][2];
 
 public:
-    static int angle_check[8];
+    static int angle_check[8]; // comparing the angles of image pose and user's pose
     int32_t Process(cv::Mat& mat);
 };
 
