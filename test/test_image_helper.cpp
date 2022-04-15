e2b718942b1f2645f11f38a47d126cb1bc81bbad
qwershu007 <qwershu007@163.com>
2022-04-15 20:15:55 +0100

    new test file

--- test/test_image_helper.cpp
+++ test/test_image_helper.cpp
@@ -6,6 +6,8 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 
+#define TEST_FILE_PATH   RESOURCE_DIR
+
 cv::VideoCapture cap;
 image_check source_test;
 
@@ -13,12 +15,12 @@ image_check source_test;
 BOOST_AUTO_TEST_SUITE(ImageCheckTest)
 BOOST_AUTO_TEST_CASE(Incorrect)
 {
-  BOOST_CHECK_EQUAL(source_test.source_check("/home/shujun/Downloads/YoagProject/test/New-demo-video.gif", cap),-1);
+  BOOST_CHECK_EQUAL(source_test.source_check(TEST_FILE_PATH "New-demo-video.gif", cap),-1);
 }
 
 BOOST_AUTO_TEST_CASE(Correct)
 {
-  BOOST_CHECK_EQUAL(source_test.source_check("/home/shujun/Downloads/YoagProject/test/test1.jpg", cap),0);
+  BOOST_CHECK_EQUAL(source_test.source_check(TEST_FILE_PATH "test1.jpg", cap),0);
 }
 BOOST_AUTO_TEST_SUITE_END()
 
