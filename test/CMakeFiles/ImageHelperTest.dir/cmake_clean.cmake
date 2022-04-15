7e9569a87630f1bb027c1bf3bbdac99c155f6943
qwershu007 <qwershu007@163.com>
2022-04-15 15:58:53 +0100

    unit_test

---
+++ test/CMakeFiles/ImageHelperTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageHelperTest.dir/test_image_helper.cpp.o"
+  "ImageHelperTest"
+  "ImageHelperTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageHelperTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test/CMakeFiles/ImageShowTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageShowTest.dir/test_image_show.cpp.o"
+  "ImageShowTest"
+  "ImageShowTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageShowTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
